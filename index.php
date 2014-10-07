cb6a840f5bc3e4824a6637a46105e03b77ba780b
Loïc Horellou <loic@formidable-studio.net>
2014-10-08 01:07:51 +0200

    update home
    + ajout fichier access pour clean URL
    + préparation du contrôleur pour l’accueil

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
+<?php
+
+	include_once( 'config.php' );
+
+?><!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Kyrielle</title>
+	<title>Kirielle</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	kyrielle
+	<h1>Kirielle</h1>
 
 	<?php
 
@@ -26,9 +30,94 @@
 
 		 */
 
-		foreach(glob("{".LOCAL_PATH.SLIDE_TEMPLATE_FOLDER."*}",GLOB_BRACE) as $folder){
+		/*foreach(glob("{".LOCAL_PATH.SLIDE_TEMPLATE_FOLDER."*}",GLOB_BRACE) as $folder){
+
+		}*/
+
+		// on utilise le fichier .htaccess pour récupérer une belle adresse au lieu d'avoir une adresse avec parametre du type ?url=nom_de_l_image
+		// ainsi le projet aura des url du type denkmal/kirielle/nom_de_l_image
+
+		if( !empty( $_GET['url'] ) )
+		{
+			$param = explode( '/', $_GET['url'] );
+
+			// pour supprimer les lignes vides
+			$param = array_filter($param, 'strlen');
+
+		?>
+
+		<h3>Chemin :</h3>
+
+		<?php
+
+			echo "<ul>";
+			foreach ($param as $key => $value) {
+				//if( !empty( $value ) )
+				echo "<li>$value</li>";
+			}
+			echo "</ul>";
+
+
+			if( count($param) >= 2 && $param[0] == 'image')
+			{
+				$imageName = $param[1];
+
+				if( is_file( LOCAL_PATH."/data/$imageName/$imageName.jpg" ) )
+				{
+					$imageURL = URL."/data/$imageName/$imageName.jpg";
+
+					echo "<img src='$imageURL'>";
+				}
+				else
+				{
+					echo "<p>L'image n'existe pas</p>";
+				}
+
+			}
+			else if( count($param) == 1 )
+			{
+				echo "<p>On est sur une page<p>";
+
+				switch ( $param[0] )
+				{
+
+					case 'a_propos' :
+
+						echo "<p>Page à propos</p>";
+
+					break;
+
+					case 'autre' :
+
+						echo "<p>Page autre</p>";
+
+					break;
+
+					case 'HD' :
+
+						echo "<p>Page HD</p>";
+
+					break;
+
+					default :
+
+						echo "<p>La page que vous cherchez n'existe pas. <a href='./'>Revenir à l'accueil</a></p>";
+						// header('Location:./');
+
+					break;
+
+				}
+
+			}	
 
 		}
+		else
+		{
+
+			echo "<p>Accueil</p>";
+
+		}
+			
 
 	?>
 
