69011bf67772bf4618a10e73818f5aee6f129471
armansansd <a.mohtadji@gmail.com>
2014-10-09 15:29:03 +0200

    front office gabarit

---
+++ front/navigation.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Kyrielle</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div id="page">
+		<div id="image" class="deroule"></div>
+		<div id="legende">Eva ambert - Strasbourg, 11 novembre 2012</div>
+		<?php include("footer.php"); ?>
+	</div>
+</body>
+</html>
